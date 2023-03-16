ae99de4be0a73e11ddb82953dc4eb34e7408f3a1
ekoki <kt.alive.0922@gmail.com>
2023-03-16 21:16:00 +0900

    条件分岐をモデルよりに修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,6 @@ class User < ApplicationRecord
 
   enum gender: { other: 0, man: 1, woman: 2 }
 
-
   scope :allowing_created_event_notification,
         -> { joins(:notification_timings).merge(NotificationTiming.created_event) }
   scope :allowing_commented_to_event_notification,
@@ -79,4 +78,9 @@ class User < ApplicationRecord
   def allow_liked_event_notification?
     notification_timings.liked_event.present?
   end
+
+  def not_attend_event?(current_user, event)
+    !(current_user.gender != 'woman' && event.only_woman == true)
+  end
+
 end
